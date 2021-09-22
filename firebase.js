bdf96083ff3d2cdff36f74e50f1b602b9ed070c4
zadigg <62830716+zadigg@users.noreply.github.com>
2021-09-22 14:44:17 +0300

    vercel change

--- firebase.js
+++ firebase.js
@@ -1,13 +1,14 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
+  apiKey: "AIzaSyCTw8WFGONpznvfhhd-Di2rREFWlFZ4wi8",
   authDomain: "googdoc-3a298.firebaseapp.com",
   projectId: "googdoc-3a298",
   storageBucket: "googdoc-3a298.appspot.com",
   messagingSenderId: "943894319691",
-  appId: process.env.APP_ID,
+  appId: "1:943894319691:web:4da9646fbbda1d9a62f3ce",
 };
+
 const app = !firebase.apps.length
   ? firebase.initializeApp(firebaseConfig)
   : firebase.app();
